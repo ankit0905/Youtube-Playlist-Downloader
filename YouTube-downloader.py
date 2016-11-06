cfd8c6db455d9b617f58e5ff60b6bec00799ec74
Ankit <ankit0905anand@gmail.com>
2016-11-07 02:27:04 +0530

    Code modified for Resuming Downloads - testing done

--- YouTube-downloader.py
+++ YouTube-downloader.py
@@ -6,9 +6,18 @@ import requests
 def downloaded(loc, filename):
     """ Check if the video is already downloaded in the required folder OR not.
     """
-    files = os.listdir(loc)
-    for _file in files:
-        if filename in _file:
+    filenames = os.listdir(loc)
+    filename = str(filename.encode('ascii','ignore'))
+    search_file=''
+    for ch in filename:
+        if ch.isalpha() or ch.isdigit():
+            search_file=search_file+ch
+    for _file in filenames:
+        file_present=''
+        for ch in _file:
+            if ch.isalpha() or ch.isdigit():
+                file_present=file_present+ch
+        if search_file in file_present:
             return True
     return False
  
@@ -16,7 +25,6 @@ def parse(url):
     """ Parse a youtube playlist page to extract links of all videos and return the links as a list.
     The url of the playlist will be given as input to the function.
     
-    The Downloading resumes if there is any interruption.
     """
     r = requests.get(url)
     soup = BeautifulSoup(r.text, "html.parser")
@@ -25,24 +33,33 @@ def parse(url):
     for ele in a_tags:
         link="https://www.youtube.com"+str(ele.get('href'))
         links.append(link)
-    links = list(set(links))
     return links
 
 def download_videos(links, loc):
     """ Download the video in the lowest mp4 quality available and store it in a folder.
         Age-restricted videos are ignored.
+        
+        The Downloading resumes if there was any interruption last time.
     """
+    print 
+    print "Press Ctrl+Z for stopping downloads at any moment.\n"
+    print "DOWNLOADING " + str(len(links)) + " VIDEOS \n"
+    ct=0
     for link in links:
-        yt = YouTube(link)
-        yt.set_filename(yt.filename)
-        print(yt.filename)
-        if downloaded(loc,yt.filename):
-            print "Already Downloaded"
-            continue
-        video = yt.filter('mp4')[0]
-        print "Downloading......."
-        #video.download(loc)
-        print "Download done."  
+        ct=ct+1
+        try:
+            yt = YouTube(link)
+            yt.set_filename(yt.filename)
+            print "Video #" + str(ct) + ": \t"+ (yt.filename)
+            if downloaded(loc,yt.filename):
+                print "\t\tAlready Downloaded \n"
+                continue
+            print "\t\tDownloading ........"
+            print "\t\tDownload Done \n"
+            video = yt.filter('mp4')[0] 
+            video.download(loc)
+        except:
+            print "Video #" + str(ct) + ": \t" + "Could Not Download" + "\n"
 
 if __name__ == '__main__':
     """ Two Command line arguments expected :
